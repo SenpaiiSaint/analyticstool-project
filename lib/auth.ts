b41e847b999ab2caa328ce76be13b5b85c052512
Sharif <saintparish6@gmail.com>
2025-04-07 17:27:31 -0400

    PrismaAdapter Update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,17 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "./prisma";
 import GithubProvider from "next-auth/providers/github";
 import { NextAuthOptions } from "next-auth";
+import { prisma } from "./prisma";
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
-  adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "database",
